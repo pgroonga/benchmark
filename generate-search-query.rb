dedf50ff53bea364d77abaa87435741678c785ee
Yasuhiro Horimoto <horimoto@clear-code.com>
2018-01-17 19:12:56 +0900

    Modify query for full text search

--- generate-search-query.rb
+++ generate-search-query.rb
@@ -1,11 +1,18 @@
-filename = "0000.jsonl"
-
-
-puts("BEGIN;")
-File.open(filename, "r") do |file|
-  file.each_line do |line|
-    value = line.match(/"title":"(?<title>.*?)"/)
-    puts("  SELECT * FROM fluentd WHERE record &@~ '#{value[:title]}';")
+values = []
+Dir.glob('*') do |item|
+  filename = item
+  File.open(filename, "r") do |file|
+    file.each_line do |line|
+      tag = line.match(/"tags":\["(?<tag>.*?)"/)
+      if tag != nil
+        values << tag[:tag]
+      end
+    end
   end
 end
+values = values.uniq
+puts("BEGIN;")
+  values.each{|value|
+    puts("  SELECT * FROM fluentd WHERE record &` 'paths == \"title\" && string @ \"#{value}\"';") 
+  }
 puts("END;")
