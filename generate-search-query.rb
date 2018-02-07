fdad2c53c176fb9579519e7336209bcbd7d2cd53
Kouhei Sutou <kou@clear-code.com>
2018-02-07 16:14:46 +0900

    Remove a trailing space

--- generate-search-query.rb
+++ generate-search-query.rb
@@ -13,6 +13,6 @@ end
 values = values.uniq
 puts("BEGIN;")
   values.each{|value|
-    puts("  SELECT * FROM fluentd WHERE record &` 'paths == \"title\" && string @ \"#{value}\"';") 
+    puts("  SELECT * FROM fluentd WHERE record &` 'paths == \"title\" && string @ \"#{value}\"';")
   }
 puts("END;")
