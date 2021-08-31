9846c31e2acfd0e5084c6e852a6b2f754a4be2ec
Sutou Kouhei <kou@clear-code.com>
2021-08-31 10:31:34 +0900

    Create partitioned table dynamically

--- lib/partitioned-table-preparer.rb
+++ lib/partitioned-table-preparer.rb
@@ -10,8 +10,8 @@ class PartitionedTablePreparer
 
     @language = options[:language] || :ja
     # https://ja.wikipedia.org/wiki/Wikipedia:%E6%97%A5%E6%9C%AC%E8%AA%9E%E7%89%88%E3%81%AE%E7%B5%B1%E8%A8%88
-    @n_records = 1_500_000
     @n_records_per_partition = options[:n_records_per_partition] || 100_000
+    @max_id = 0
 
     @wikipedia = Datasets::Wikipedia.new(language: @language)
     @pages = @wikipedia.each
@@ -25,26 +25,17 @@ class PartitionedTablePreparer
   private
   def prepare_schema
     Psql.open(@database_name) do |psql|
-      response = ""
-      response << psql.execute(<<-SQL)
+      psql.execute(<<-SQL)
 DROP TABLE IF EXISTS pages;
       SQL
-      response << psql.execute(<<-SQL)
+      psql.execute(<<-SQL)
 CREATE TABLE pages (
   id int PRIMARY KEY,
   title text not null,
   content text
 ) PARTITION BY RANGE (id);
       SQL
-      offsets = 0.step(@n_records, @n_records_per_partition).each
-      offsets.each_cons(2) do |start, next_start|
-        response << psql.execute(<<-SQL)
-CREATE TABLE pages_#{start}_#{next_start}
-  PARTITION OF pages
-  FOR VALUES FROM (#{start}) TO (#{next_start});
-        SQL
-      end
-      response << psql.finish
+      psql.finish
     end
   end
 
@@ -52,6 +43,17 @@ CREATE TABLE pages_#{start}_#{next_start}
     text_column = Column.new("name", "text")
     Psql.open(@database_name) do |psql|
       @wikipedia.each do |page|
+        until page.id < @max_id
+          start = @max_id
+          next_start = @max_id + @n_records_per_partition
+          psql.execute(<<-SQL)
+CREATE TABLE pages_#{start}_#{next_start}
+  PARTITION OF pages
+  FOR VALUES FROM (#{start}) TO (#{next_start});
+          SQL
+          @max_id = next_start
+        end
+
         psql.execute(<<-SQL)
 INSERT INTO pages
   VALUES (#{page.id},
